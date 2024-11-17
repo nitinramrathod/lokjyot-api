6395956868ba96555b60dd2f88db9611512a74c7
nitinrathod.tg <nitinrathod.tg@gmail.com>
2024-11-17 06:43:38 +0000

    some changes in model

--- src/models/expense.model.js
+++ src/models/expense.model.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
 const ExpenseSchema = new mongoose.Schema({
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, trim: true },
+    name: { type: String, required: true, trim: true },
     mobile: { type: String, required: true, trim: true },
     amount: { type: String, required: true, trim: true },
     duration: { type: String, required: true, trim: true },
-    startDate: { type: String, trim: true },
     endDate: { type: String, trim: true },
     description: { type: String, trim: true },
 })
